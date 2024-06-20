1c0c7db2792e72daa9ef21ba53d44a0be6f15b88
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-06-20 13:59:14 +0800

    Created routes for aboutMe, skills, and hobbies.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/aboutMe', function () {
+    return view('aboutMe');
+});
+
+Route::get('/skills', function () {
+    return view('skills');
+});
+
+Route::get('/hobbies', function () {
+    return view('hobbies');
+});
