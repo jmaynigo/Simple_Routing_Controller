5820a5e5c9d867f4bab5216e2ff84c354542eb9a
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-06-20 22:29:25 +0800

    Creating aboutMe view and displaying aboutMe data.

--- resources/views/aboutMe.blade.php
+++ resources/views/aboutMe.blade.php
@@ -1,3 +1,27 @@
 <div>
-    <!-- I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison -->
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>About Me</title>
+        </head>
+        <body>
+            <h1>{{ $title }}</h1>
+            <h3>Hi! I'm <b>{{ $name }}</b> from {{ $address }}.</h3>
+            <p>I am currently {{ $age }} years old, born on {{ $birthday }}.</p>
+            <p>My dream was to become a {{ $dream }} but I am currently studying to become an {{ $currentCareerPath }}.</p>
+        
+            <button>
+                <a href="">About Me</a>
+            </button>
+            <button>
+                <a href="">Skills</a>
+            </button>
+            <button>
+                <a href="">Hobbies</a>
+            </button>
+            
+        </body>
+    </html>
 </div>
