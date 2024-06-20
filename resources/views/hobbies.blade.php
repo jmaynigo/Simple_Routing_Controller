eab84bd3d98dc975c9c893c6694c20c7bec5fe2a
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-06-20 22:29:43 +0800

    Creating hobbies view and displaying hobbies data.

--- resources/views/hobbies.blade.php
+++ resources/views/hobbies.blade.php
@@ -1,3 +1,31 @@
 <div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Hobbies</title>
+        </head>
+        <body>
+            <h1>{{ $title }}</h1>
+            <ul>
+                <li>{{ $hobby1 }}</li>
+                <li>{{ $hobby2 }}</li>
+                <li>{{ $hobby3 }}</li>
+                <li>{{ $hobby4 }}</li>
+                <li>{{ $hobby5 }}</li>
+            </ul>
+
+            <button>
+                <a href="">About Me</a>
+            </button>
+            <button>
+                <a href="">Skills</a>
+            </button>
+            <button>
+                <a href="">Hobbies</a>
+            </button>
+
+        </body>
+    </html>
 </div>
