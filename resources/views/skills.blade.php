6dbffaa67cd16bcf1794bd8655f0b746d0574aa9
jmaynigo <jpm2242@students.uc-bcf.edu.ph>
2024-06-20 22:30:02 +0800

    Creating skills view and displaying skills data.

--- resources/views/skills.blade.php
+++ resources/views/skills.blade.php
@@ -1,3 +1,30 @@
 <div>
-    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
-</div>
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Skills</title>
+        </head>
+        <body>
+            <h1>{{ $title }}</h1>
+            <ul>
+                <li>{{ $skills1 }}</li>
+                <li>{{ $skills2 }}</li>
+                <li>{{ $skills3 }}</li>
+                <li>{{ $skills4 }}</li>
+                <li>{{ $skills5 }}</li>
+            </ul>
+
+            <button>
+                <a href="">About Me</a>
+            </button>
+            <button>
+                <a href="">Skills</a>
+            </button>
+            <button>
+                <a href="">Hobbies</a>
+            </button>
+
+        </body>
+    </html></div>
